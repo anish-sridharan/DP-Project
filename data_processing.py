2edca1aa479f63fe63136a044530243fc93dba1d
anish-sridharan <anishsridharan26797@gmail.com>
2021-07-05 16:41:49 +0200

    processed data

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
 import json
 from collections import Counter
-with open('project_3_data.json') as f:
+with open('project_3_data_sorted.json') as f:
   player_data = json.load(f)
 player_id=0
 while player_id<len(player_data):
@@ -22,13 +22,29 @@ while player_id<len(player_data):
 
         temp_player_data.append(player_data[player_id]['history'][i].copy() ) 
     player_data[player_id]['history']=temp_player_data
+    
     player_id+=1
 player_id=0
-
+position_id=0
+position_player_id=0
+previous_position=player_data[0]['position']
 while player_id<len(player_data):
   previous_year=0
   temp_player_data=[]
-  
+  player_data[player_id]['player_id']=player_id
+  if(player_id==0):
+    position_id=0
+    position_player_id=0
+    previous_position=player_data[player_id]['position']
+  elif(player_id!=0):
+    if player_data[player_id]['position']!=previous_position:
+      position_id+=1
+      position_player_id=0
+    else:
+      position_player_id+=1
+    previous_position=player_data[player_id]['position']
+  player_data[player_id]['position_id']=position_id
+  player_data[player_id]['position_player_id']=position_player_id
   for years in range (len(player_data[player_id]['history'])):
 
     year=int(player_data[player_id]['history'][years]['date'].split( )[2])
@@ -47,4 +63,6 @@ while player_id<len(player_data):
 
 k = Counter(name['position'] for name in player_data)
 print(k)
+outfile= "player_data_processed.json"
 
+json.dump(player_data,open(outfile,"w"),indent=4)
